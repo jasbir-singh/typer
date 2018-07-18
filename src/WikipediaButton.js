86824bff9f9767990abb84943609cbadd111e3d1
Jasbir Singh <jasbir_singh@live.co.uk>
2018-07-18 13:04:53 +0100

    Style the components

--- src/WikipediaButton.js
+++ src/WikipediaButton.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
 import {
   fetchRandomArticle,
 } from './actions';
 
+const StyledWikipediaButton = styled.div`
+  margin: 10% 0;
+`;
+
 const WikipediaButton = ({ fetchRandomArticle }) => {
   return (
+    <StyledWikipediaButton>
       <button onClick={fetchRandomArticle}>Fetch random Wiki Article</button>
+    </StyledWikipediaButton>
   );
 };
 
