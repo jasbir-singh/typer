aa8458c1477ac488bdf2c7a9edd149c90db200f1
Jasbir Singh <jasbir_singh@live.co.uk>
2018-09-12 02:42:49 +0100

    Fix typo

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,12 @@
 import * as API from './api';
 export const TYPE_STARTED = 'TYPE_STARTED';
-export const TYPE_SUCCESS = 'TYPE_SUCESS';
+export const TYPE_SUCCESS = 'TYPE_SUCCESS';
 export const TYPE_FAIL = 'TYPE_FAIL';
 export const UPDATE_TYPING_STATS = 'UPDATE_TYPING_STATS';
 export const TYPE_FINISHED = 'TYPE_FINISHED';
 export const FETCH_RANDOM_ARTICLE_SUCCESS = 'FETCH_RANDOM_ARTICLE_SUCCESS';
 export const FETCH_RANDOM_ARTICLE = 'FETCH_RANDOM_ARTICLE';
 
-// https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow
 const typeStarted = () => ({
   type: TYPE_STARTED,
   payload: {
